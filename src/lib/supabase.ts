2c623c2fb11b48a8b1078b34d4d4bd5c8bcb978d
Jonathan Perrinet <jonathan.perrinet@epita.fr>
2026-05-17 17:31:34 +0200

    feat: add Supabase TypeScript definitions and apply type-safe client initialization

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from './supabaseTypes';
 
 const supabaseUrl = `https://${import.meta.env.VITE_SUPABASE_PROJECTID}.supabase.co`;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLIC_KEY;
 // const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
